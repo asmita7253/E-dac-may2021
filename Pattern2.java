e5708811502f320e8511f087d8b7f9d5f845a6a0
asmita7253 <75712345+asmita7253@users.noreply.github.com>
2021-05-11 16:09:03 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,15 @@
+class Pattern2{
+public static void main(String args[]){
+int numCount=1;
+int num=1;
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=numCount;j++)
+{
+System.out.print(num);
+}
+System.out.println();
+num++;
+numCount++;
+}
+}}
