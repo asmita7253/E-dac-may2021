74b5f04050a4536a65721e70c47e1d56cbcd7715
asmita7253 <75712345+asmita7253@users.noreply.github.com>
2021-05-11 16:10:48 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,12 @@
+class pattern3{
+public static void main(String []args){
+int starCount=1;
+for(int i=1;i<=5;i++)
+{
+for (int j=1;j<=starCount;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+starCount++;
+}}}
